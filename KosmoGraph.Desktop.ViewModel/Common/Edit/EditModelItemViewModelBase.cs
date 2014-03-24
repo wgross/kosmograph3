5355e7face2704c9e4dca98386be3bd0234aeb33
Wolfgang Gross <w.gross@text-mode.de>
2014-03-24 13:56:07 +0100

    CHANGE: Introduced virtual PrepareComitImplementation at editor base class

--- KosmoGraph.Desktop.ViewModel/Common/Edit/EditModelItemViewModelBase.cs
+++ KosmoGraph.Desktop.ViewModel/Common/Edit/EditModelItemViewModelBase.cs
@@ -1,53 +1,54 @@
 ﻿namespace KosmoGraph.Desktop.ViewModel
 {
     using Microsoft.Practices.Prism.Commands;
-    using Microsoft.Practices.Prism.ViewModel;
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public abstract class EditModelItemViewModelBase : ModelItemViewModelBase
     {
+        #region Construction and initialization of this instance
+
         public EditModelItemViewModelBase(EntityRelationshipViewModel model)
             : base(model)
         {
+            this.PrepareCommit = new DelegateCommand(this.ExecutePrepareCommit, this.CanExecutePrepareCommit);
             this.Commit = new DelegateCommand(this.ExecuteCommit, this.CanExecuteCommit);
             this.Rollback = new DelegateCommand(this.ExecuteRollback, this.CanExecuteRollback);
         }
 
+        #endregion
+
+        #region Validate/Prepare Commit Editor
+
+        public DelegateCommand PrepareCommit { get; private set; }
+
+        virtual protected void ExecutePrepareCommit() { }
+
+        virtual protected bool CanExecutePrepareCommit() { return false; }
+
+        #endregion
+
         #region Commit Editor
 
-        public DelegateCommand Commit
-        {
-            get;
-            private set;
-        }
+        public DelegateCommand Commit { get; private set; }
 
         abstract protected void ExecuteCommit();
-        
+
         abstract protected bool CanExecuteCommit();
-        
+
         #endregion
 
         #region Rollback Editor
 
-        public DelegateCommand Rollback
-        {
-            get;
-            private set;
-        }
+        public DelegateCommand Rollback { get; private set; }
 
         abstract protected void ExecuteRollback();
-       
+
         abstract protected bool CanExecuteRollback();
-        
+
         #endregion
 
         protected void RefreshCommands()
         {
+            this.PrepareCommit.RaiseCanExecuteChanged();
             this.Commit.RaiseCanExecuteChanged();
             this.Rollback.RaiseCanExecuteChanged();
         }
